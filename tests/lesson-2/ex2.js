0c62465d8e897f0d09805e3616d0765f648846e7
Paul <lethanhhoa8818@gmail.com>
2025-10-19 17:54:25 +0700

    Execute Bai Tap Khai Bao Bien Va Dieu Kien lesson 2

---
+++ tests/lesson-2/ex2.js
@@ -0,0 +1,3 @@
+const myName = "Alex";
+// myName = "Nagi"; vì ở dòng 1 là hằng số const, không thể gán lại giá trị mới như let và var. Nên ở dòng 3 sẽ ra kết quả của dòng 1 là Alex
+console.log(myName);
---
+++ tests/lesson-2/ex4.js
@@ -0,0 +1,5 @@
+const height = 175
+const goodWeight = 75 * 9 / 10
+const maxWeight = 75
+const minWeight = 75 * 8 / 10
+console.log("Cân nặng lý tưởng: ",goodWeight , "," , "Cân nặng tối đa: ",maxWeight , "," , "Cân nặng tối thiểu: ",minWeight)
