ce955013680b49d1fff7e70986197c5285200741
Paul <lethanhhoa8818@gmail.com>
2025-10-23 21:11:47 +0700

    init project

---
+++ tests/lesson-2/ex10.js
@@ -0,0 +1,11 @@
+let student = {
+    name : "Alex", 
+    class : {
+        name : "K19",
+        major : "Automation"
+    },
+    finalGrade : "8.5"
+}
+    console.log(student.finalGrade);
+    console.log(student.class.name);
+    console.log(student["class"]["name"])
